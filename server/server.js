5059dfd855a0b51a8386b40d14c339790e2e987b
Difficins <a.kolomijec@seznam.cz>
2024-01-24 17:23:03 +0100

    Added cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const workoutRoutes = require("./routes/workouts");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
