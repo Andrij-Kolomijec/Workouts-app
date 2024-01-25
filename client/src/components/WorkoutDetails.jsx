5bafeb7dbf937173f1103e75911460f65681989b
Difficins <a.kolomijec@seznam.cz>
2024-01-25 14:56:59 +0100

    Added delete method

--- client/src/components/WorkoutDetails.jsx
+++ client/src/components/WorkoutDetails.jsx
@@ -1,7 +1,19 @@
 import classes from "./WorkoutDetails.module.css";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { formatDistanceToNow } from "date-fns";
 import PropTypes from "prop-types";
 
 export default function WorkoutDetails({ workout }) {
+  const { dispatch } = useWorkoutsContext();
+
+  async function handleClick() {
+    const response = await fetch(import.meta.env.VITE_PORT + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+    if (response.ok) dispatch({ type: "DELETE_WORKOUT", payload: json });
+  }
+
   return (
     <div className={classes.workout}>
       <h3>{workout.title}</h3>
@@ -13,7 +25,12 @@ export default function WorkoutDetails({ workout }) {
         <strong>Reps: </strong>
         {workout.reps}
       </p>
-      <p>{workout.createdAt}</p>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        Delete
+      </span>
     </div>
   );
 }
