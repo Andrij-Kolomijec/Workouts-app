991abc9043b4bbfb3aae0629510bb3215281bf09
Difficins <a.kolomijec@seznam.cz>
2024-01-28 12:14:33 +0100

    Set global workouts state to null on logout

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -1,4 +1,5 @@
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutsContext } from "./useWorkoutsContext";
 
 export function useLogout() {
   // to log out (no need for server):
@@ -6,9 +7,14 @@ export function useLogout() {
 
   const { dispatch } = useAuthContext();
 
+  // so the workouts from previous users (global states)
+  // do not flash before rendering the specific users workouts
+  const { dispatch: workoutsDispatch } = useWorkoutsContext();
+
   const logout = () => {
     localStorage.removeItem("user");
     dispatch({ type: "LOGOUT" });
+    workoutsDispatch({ type: "SET_WORKOUTS", payload: null });
   };
 
   return { logout };
